845c7a57985e1d37ba50d6c8bf2747d8ce88f8ba
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-16 12:10:02 +0200

    Added Hello World!

--- src/main/java/com/powerteam/myapi/MyapiApplication.java
+++ src/main/java/com/powerteam/myapi/MyapiApplication.java
@@ -3,8 +3,18 @@ package com.powerteam.myapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
 @SpringBootApplication
 public class MyapiApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyapiApplication.class, args);

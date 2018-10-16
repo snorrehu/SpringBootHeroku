c4620ad6168ab126008bf0f8974de5fa0de7ac8c
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-16 15:37:24 +0200

    Added player resource handling

--- src/main/java/resources/PlayerRepository.java
+++ src/main/java/resources/PlayerRepository.java
@@ -1,10 +1,6 @@
 package resources;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 

3961bf8e8f53f9a2021c910bb1903f096bd622f0
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-16 11:09:25 +0200

    first commit

---
+++ src/test/java/com/powerteam/myapi/MyapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.powerteam.myapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
